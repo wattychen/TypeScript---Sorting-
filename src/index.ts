07c5b2e7c08bc02af33f538127b7c56c115699fe
Watson Chen <wc.watsonchen@gmail.com>
2022-12-14 20:54:57 -0500

    refactored code to allow LinkedList and Characters collections to extend from Sorter class

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,17 @@ import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([1, 99, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Hello");
-const stringSorter = new Sorter(charactersCollection);
-stringSorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(100);
 linkedList.add(4);
 linkedList.add(301);
+linkedList.sort();
 
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
 linkedList.print();
